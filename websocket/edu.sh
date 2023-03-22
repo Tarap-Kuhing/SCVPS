b13f460532895d0eb850d34058cdbd2779307775
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-03-22 18:10:15 +0800

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 # Proxy For Edukasi & Imclass
-# Tarap Kuhing
+# Mod By TARAP KUHING
 # ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/Tarap-Kuhing/perizinan/main/ipvps.txt | grep $MYIP )
+if [ $MYIP = $MYIP ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Please Contact Admin!!"
+echo -e "${NC}${LIGHT}Facebook : "
+echo -e "${NC}${LIGHT}WhatsApp : 085754292950"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/Hendra2012"
+exit 0
+fi
 
-# Link Hosting Kalian
-tarapkuhing="https://github.com/Tarap-Kuhing/SCVPS/main/websocket"
-
-# Getting Proxy Template
-wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/websocket/websocket.py
-chmod +x /usr/local/bin/ws-nontls
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/websocket/ws-dropbear
+chmod +x /usr/local/bin/ws-dropbear
 
 # Installing Service
 cat > /etc/systemd/system/ws-nontls.service << END
 [Unit]
-Description=Python Proxy Mod By Tarap Kuhing
-Documentation=https://nekopi.care
+Description=Python Proxy Mod By TARAP KUHING
+Documentation=https://t.me/Hendra2012
 After=network.target nss-lookup.target
 
 [Service]
@@ -23,7 +43,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 8880
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-dropbear
 Restart=on-failure
 
 [Install]
@@ -31,18 +51,18 @@ WantedBy=multi-user.target
 END
 
 systemctl daemon-reload
-systemctl enable ws-nontls
-systemctl restart ws-nontls
+systemctl enable ws-dropbear.service
+systemctl start ws-dropbear.service
+systemctl restart ws-dropbear.service
 
-# Getting Proxy Template
-wget -q -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/websocket/ws-ovpn.py
+wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/websocket/ws-ovpn.py
 chmod +x /usr/local/bin/ws-ovpn
 
 # Installing Service
 cat > /etc/systemd/system/ws-ovpn.service << END
 [Unit]
-Description=Python Proxy Mod By Tarap Kuhing
-Documentation=https://nekopoi.care
+Description=Python Proxy Mod By TARAP KUHING
+Documentation=https://t.me/Hendra2012
 After=network.target nss-lookup.target
 
 [Service]
@@ -62,15 +82,14 @@ systemctl daemon-reload
 systemctl enable ws-ovpn
 systemctl restart ws-ovpn
 
-# Getting Proxy Template
-wget -q -O /usr/local/bin/ws-tls https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/websocket/ws-tls
-chmod +x /usr/local/bin/ws-tls
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/sshws/ws-stunnel
+chmod +x /usr/local/bin/ws-stunnel
 
 # Installing Service
-cat > /etc/systemd/system/ws-tls.service << END
+cat > /etc/systemd/system/ws-stunnel.service << END
 [Unit]
-Description=Python Proxy Mod By Tarap Kuhing
-Documentation=https://nekopoi.care
+Description=Python Proxy Mod By TARAP KUHING
+Documentation=https://t.me/Hendra2012
 After=network.target nss-lookup.target
 
 [Service]
@@ -79,13 +98,13 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 443
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-stunnel
 Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
 END
 
-systemctl daemon-reload
-systemctl enable ws-tls
-systemctl restart ws-tls
+systemctl enable ws-stunnel.service
+systemctl start ws-stunnel.service
+systemctl restart ws-stunnel.service
