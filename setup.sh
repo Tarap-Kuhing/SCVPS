b2af6b8396b55e4c2785f31e7a26c25132ed0bb7
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-03-22 20:11:02 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -133,9 +133,9 @@ echo "   - SSTP VPN                : 444"  | tee -a log-install.txt
 echo "   - Shadowsocks-R           : 1443-1543"  | tee -a log-install.txt
 echo "   - SS-OBFS TLS             : 2443-2543"  | tee -a log-install.txt
 echo "   - SS-OBFS HTTP            : 3443-3543"  | tee -a log-install.txt
-echo "   - XRAYS Vmess TLS         : 8443"  | tee -a log-install.txt
+echo "   - XRAYS Vmess TLS         : 443"  | tee -a log-install.txt
 echo "   - XRAYS Vmess None TLS    : 80"  | tee -a log-install.txt
-echo "   - XRAYS Vless TLS         : 8443"  | tee -a log-install.txt
+echo "   - XRAYS Vless TLS         : 443"  | tee -a log-install.txt
 echo "   - XRAYS Vless None TLS    : 80"  | tee -a log-install.txt
 echo "   - XRAYS Trojan            : 2083"  | tee -a log-install.txt
 echo "   - XRAYS VMESS GRPC        : 1180"  | tee -a log-install.txt
@@ -168,132 +168,5 @@ echo "   - White Label" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo " Reboot 15 Sec"
 sleep 15
-rm -f setup.sh
-reboot
-rm -f setup.sh
-clear
-if [ -f "/etc/xray/domain" ]; then
-echo "Script Already Installed"
-exit 0
-fi
-mkdir /var/lib/crot;
-echo "IP=" >> /var/lib/crot/ipvps.conf
-wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/ssh/slhost.sh && chmod +x slhost.sh && ./slhost.sh
-#install xray
-wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/xray/ins-xray.sh && chmod +x ins-xray.sh && screen -S xray ./ins-xray.sh
-#install ssh ovpn
-wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh ./ssh-vpn.sh
-#Install sstp
-wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/sstp/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
-#install ssr
-wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/ssr/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-#
-wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/shadowsocks/sodosok.sh && chmod +x sodosok.sh && screen -S shadowsocks ./sodosok.sh
-#installwg
-wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/wireguard/wg.sh && chmod +x wg.sh && screen -S wireguard ./wg.sh
-#install L2TP
-wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/ipsec/psec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
-#
-wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/backup/set-br.sh && chmod +x set-br.sh && ./set-br.sh
-# Websocket
-wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/websocket/edu.sh && chmod +x edu.sh && ./edu.sh
-# Ohp Server
-wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/ohp/ohp.sh && chmod +x ohp.sh && ./ohp.sh
-# Install Slowdns Server
-wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/SLDNS/install-sldns && chmod +x install-sldns && ./install-sldns
-# Informasi IP Saya dan Semua Port TCP UDP
-wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/ipsaya.sh && chmod +x ipsaya.sh
-# install xray grpc
-wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/grpc/sl-grpc.sh && chmod +x sl-grpc.sh && ./sl-grpc.sh
-
-rm -f /root/slhost.sh
-rm -f /root/ins-xray.sh
-rm -f /root/ssh-vpn.sh
-rm -f /root/sstp.sh
-rm -f /root/ssr.sh
-rm -f /root/sodosok.sh
-rm -f /root/wg.sh
-rm -f /root/ipsec.sh
-rm -f /root/set-br.sh
-rm -f /root/edu.sh
-rm -f /root/ohp.sh
-rm -f /root/install-sldns
-rm -f /root/ipsaya
-rm -f /root/sl-grpc.sh
-cat <<EOF> /etc/systemd/system/autosett.service
-[Unit]
-Description=autosetting
-Documentation=nekopoi.care
-
-[Service]
-Type=oneshot
-ExecStart=/bin/bash /etc/set.sh
-RemainAfterExit=yes
-
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl daemon-reload
-systemctl enable autosett
-wget -O /etc/set.sh "https://${akbarvpn}/set.sh"
-chmod +x /etc/set.sh
-history -c
-echo "1.2" > /home/ver
-echo " "
-echo "Installation has been completed!!"echo " "
-echo "============================================================================" | tee -a log-install.txt
-echo "" | tee -a log-install.txt
-echo "----------------------------------------------------------------------------" | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "   >>> Service & Port"  | tee -a log-install.txt
-echo "   - OpenSSH                 : 22, 2253"  | tee -a log-install.txt
-echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 990"  | tee -a log-install.txt
-echo "   - Stunnel5                : 443, 445"  | tee -a log-install.txt
-echo "   - Dropbear                : 443, 109, 143"  | tee -a log-install.txt
-echo "   - Squid Proxy             : 3128, 8080"  | tee -a log-install.txt
-echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
-echo "   - Nginx                   : 89"  | tee -a log-install.txt
-echo "   - Wireguard               : 7070"  | tee -a log-install.txt
-echo "   - L2TP/IPSEC VPN          : 1701"  | tee -a log-install.txt
-echo "   - PPTP VPN                : 1732"  | tee -a log-install.txt
-echo "   - SSTP VPN                : 444"  | tee -a log-install.txt
-echo "   - Shadowsocks-R           : 1443-1543"  | tee -a log-install.txt
-echo "   - SS-OBFS TLS             : 2443-2543"  | tee -a log-install.txt
-echo "   - SS-OBFS HTTP            : 3443-3543"  | tee -a log-install.txt
-echo "   - XRAYS Vmess TLS         : 8443"  | tee -a log-install.txt
-echo "   - XRAYS Vmess None TLS    : 80"  | tee -a log-install.txt
-echo "   - XRAYS Vless TLS         : 8443"  | tee -a log-install.txt
-echo "   - XRAYS Vless None TLS    : 80"  | tee -a log-install.txt
-echo "   - XRAYS Trojan            : 8443"  | tee -a log-install.txt
-echo "   - XRAYS VMESS GRPC        : 1180"  | tee -a log-install.txt
-echo "   - XRAYS VLESS GRPC        : 2280"  | tee -a log-install.txt
-echo "   - CloudFront Websocket    : "  | tee -a log-install.txt
-echo "   - Websocket TLS           : 443"  | tee -a log-install.txt
-echo "   - Websocket None TLS      : 8880"  | tee -a log-install.txt
-echo "   - Websocket Ovpn          : 2086"  | tee -a log-install.txt
-echo "   - OHP SSH                 : 8181"  | tee -a log-install.txt
-echo "   - OHP Dropbear            : 8282"  | tee -a log-install.txt
-echo "   - OHP OpenVPN             : 8383"  | tee -a log-install.txt
-echo "   - TrojanGo                : 2087"  | tee -a log-install.txt
-echo "   - SLOWDNS OpenSSH         : 2253[OFF]"  | tee -a log-install.txt
-echo "   - SLOWDNS Dropbear        : 1153[OFF]"  | tee -a log-install.txt
-echo "   - SLOWDNS SSL/TLS         : 3353[OFF]"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
-echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
-echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
-echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
-echo "   - IPtables                : [ON]"  | tee -a log-install.txt
-echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
-echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
-echo "   - Autoreboot On 05.00 GMT +7" | tee -a log-install.txt
-echo "   - Autobackup Data" | tee -a log-install.txt
-echo "   - Restore Data" | tee -a log-install.txt
-echo "   - Auto Delete Expired Account" | tee -a log-install.txt
-echo "   - Full Orders For Various Services" | tee -a log-install.txt
-echo "   - White Label" | tee -a log-install.txt
-echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
-echo " Reboot 15 Sec"
-sleep 10
 rm -f setup.sh
 reboot
